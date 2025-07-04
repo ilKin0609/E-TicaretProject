621f2b287a574a584be02005a680c305169b2f62
ilKin0609 <ilkinmammadov.11@icloud.com>
2025-07-04 10:03:30 +0400

    Null commit

--- src/Core/E-Ticaret Project.Application/DTOs/UserAuthenticationDtos/UserRegisterDto.cs
+++ src/Core/E-Ticaret Project.Application/DTOs/UserAuthenticationDtos/UserRegisterDto.cs
@@ -4,7 +4,6 @@ public record UserRegisterDto(
 
     string FullName,
     string Email,
-    string? ProfileImageUrl,
     string Password,
     string Role
 );
--- src/Core/E-Ticaret Project.Application/Validations/CategoryValidations/CategoryCreateDtoValidator.cs
+++ src/Core/E-Ticaret Project.Application/Validations/CategoryValidations/CategoryCreateDtoValidator.cs
@@ -5,5 +5,14 @@ namespace E_Ticaret_Project.Application.Validations.CategoryValidations;
 
 public class CategoryCreateDtoValidator:AbstractValidator<CategoryCreateDto>
 {
-    public CategoryCreateDtoValidator() { }
+    public CategoryCreateDtoValidator() {
+
+        RuleFor(Ct => Ct.Name)
+            .NotEmpty().WithMessage("Category cannot be null")
+            .MaximumLength(100).WithMessage("Category name must be at most 100 characters.");
+
+        RuleFor(Ct => Ct.ParentCategoryId)
+            .NotEqual(Guid.Empty).When(Ct => Ct.ParentCategoryId.HasValue)
+            .WithMessage("ParentCategoryId must be a valid GUID.");
+    }
 }
